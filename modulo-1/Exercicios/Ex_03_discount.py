065126678a6120f3988855fe352052bdb77c7a8c
FabricioHA <fabricio.27767@gmail.com>
2024-03-06 16:23:55 -0300

    Add new files module 1

---
+++ modulo-1/Exercicios/Ex_03_discount.py
@@ -0,0 +1,22 @@
+"""
+Desenvolva um algoritmo que solicite ao usuário o preço de um produto e um percentual de desconto a ser aplicado a ele. Calcule e exiba o valor do 
+desconto e o preço final do produto.
+"""
+print("==============Descontão Relampago! Cadastre-se já e aproveite!==============\n")
+
+while True:
+    price = input("Digite o preço do seu produto: ")
+    discountPercentual = input("Digite o percentual de desconto a ser aplicado (0/100)%: ")
+
+    if not price.replace(".", "").isnumeric() or not discountPercentual.replace(".", "").isnumeric():
+        print("\nValor inserido é invalido. Digite novamente\n")
+
+    else:
+        price = float(price)
+        discountPercentual = float(discountPercentual)
+        break
+
+discountValue = price * (discountPercentual/100)
+finalPrice = price - discountValue
+
+print("\nDesconto aplicado: R$%.2f\nPreço final: %.2f" %(discountValue, finalPrice))
---
+++ modulo-1/Exercicios/Ex_04_converter_temp.py
@@ -0,0 +1,16 @@
+"""
+Desenvolva um algoritmo que converta uma temperatura em Celsius (C) para Fahrenheit (F). A equação de conversão é:
+"""
+
+while True:
+
+    celsius = input("Digite a temperatura em Celsius (C°): ")
+    
+    if not celsius.replace(".", "").isnumeric():
+        print("Valores invalidos")
+    
+    celsius = float(celsius)
+
+fahrenheit = (9*celsius)/5+32
+
+print("A temperatura de %.2fC° convertida em Fahrenheit é de %.2fF°" %(celsius, fahrenheit))
